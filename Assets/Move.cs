ac9f941bda2d2a28cde59e566c08304582f2f6dc
Anoop Kumar Sharma <anoopkrsh22@gmail.com>
2022-08-22 04:16:06 +0530

    semi final

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,50 +1,35 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEngine.EventSystems.EventTrigger;
-using static UnityEngine.GraphicsBuffer;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
+
+public enum Mass
+{
+    Mercury,
+    Venus,
+    Earth,
+    Mars,
+    Jupiter,
+    Saturn,
+    Uranus,
+    Neptune
+}
 
 public class Move : MonoBehaviour
 {
     public GameObject cameraObj;
     public GameObject stringObj;
-    public float yAngle;
-    public enum Mass
-    {
-        Mercury,
-        Venus,
-        Earth,
-        Mars,
-        Jupiter,
-        Saturn,
-        Uranus,
-        Neptune
-    }
-    [Range(1,10)]public int Height;
-    [Range(0,90)]public int Angle;
-    [Range(0,10)]public int Drag;
-    public Mass Planet;
-    private Mass tempPlanet;
-    private int tempHeight;
-    private int tempAngle;
-    private int tempDrag;
+    private Mass Planet;
     private float masses;
     [SerializeField]HingeJoint joint;
     [SerializeField]Rigidbody rb;
     Vector3 anchor;
-    [SerializeField] bool useGravity;
-
-
     public float speedH = 2.0f;
-    public float speedV = 2.0f;
     private float yaw = 0.0f;
-    public bool RotatePlane;
+    private bool RotatePlane;
 
 
     public Slider AngleSlide;
@@ -58,14 +43,10 @@ public class Move : MonoBehaviour
 
     public TMP_Dropdown planetDroupDown;
     public TMP_Text planetNamesText;
+
+    public Toggle rotateToggle;
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
-        //joint = GetComponent<HingeJoint>();
-        tempHeight = Height;
-        tempAngle = Angle;
-        tempPlanet = Planet;
-        tempDrag = Drag;
         ChangeMass(Planet);
         PopulateList();
     }
@@ -76,10 +57,18 @@ public class Move : MonoBehaviour
         List<string> planetName= new List<string>(planetNamesData);
         planetDroupDown.AddOptions(planetName);
     }
+    public void RotatePlaneActive()
+    {
+        RotatePlane = !RotatePlane;
+        if (!RotatePlane)
+        {
+            cameraObj.transform.eulerAngles = new Vector3(0, 0, 0.0f);
+        }
+    }
     public void ChangeMasses()
     {
-        tempPlanet = (Mass)planetDroupDown.value;
-        ChangeMass(tempPlanet);
+        Planet = (Mass)planetDroupDown.value;
+        ChangeMass(Planet);
     }
     private void ChangeMass(Mass planet)
     {
@@ -123,66 +112,48 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ChangeDrag();
-        //AngleChange();
-        //HeightChange();
-        if (useGravity)
-        {
-            useGravity = false;
-            rb.useGravity = true;
-        }
         if (RotatePlane)
         {
             yaw += speedH * Input.GetAxis("Mouse X");
         }
+
+    }
+
+    public void ActivateGravity()
+    {
+        rb.useGravity = !rb.useGravity;
     }
 
     public void ChangeDrag()
     {
         rb.drag = DragSlide.value;
         sliderDragText.text = "Drag: "+ DragSlide.value.ToString("0.0");
-        //if (tempDrag != Drag)
-        //{
-        //    rb.drag = Drag / 10f;
-        //    tempDrag = Drag;
-        //}
     }
 
     public void HeightChange()
     {
-        stringObj.transform.localScale = new Vector3(stringObj.transform.localScale.x, HeightSlide.value, stringObj.transform.localScale.z);
+        AngleChange();
+        stringObj.transform.localScale = new Vector3(stringObj.transform.localScale.x, HeightSlide.value/10, stringObj.transform.localScale.z);
         anchor = joint.anchor;
         joint.anchor = anchor;
         sliderHeightText.text = "Height: " + HeightSlide.value.ToString("00");
-
-        //if (tempHeight != Height)
-        //{
-        //    stringObj.transform.localScale = new Vector3(stringObj.transform.localScale.x, tempHeight, stringObj.transform.localScale.z);
-        //    anchor = joint.anchor;
-        //    joint.anchor = anchor;
-        //    tempHeight = Height;
-        //}
     }
 
     public void AngleChange()
     {
-        
         stringObj.transform.localRotation = Quaternion.Euler(AngleSlide.value, 0, 0);
         sliderAngleText.text = "Angle: " + AngleSlide.value.ToString("00"+"°");
-        //if (tempAngle != Angle) 
-        //{
-        //    stringObj.transform.localRotation = Quaternion.Euler(Angle, 0, 0);
-
-        //    tempAngle = Angle;
-        //}
     }
 
     private void FixedUpdate()
     {
-        if (!RotatePlane)
-        {
-            cameraObj.transform.eulerAngles = new Vector3(0, 0, 0.0f);
-        }
+        if (!RotatePlane) return;
         cameraObj.transform.eulerAngles = new Vector3(0, yaw, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(anchor, .01f);
     }
 }
